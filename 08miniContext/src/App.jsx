1d0ed85ef43bb2b766c4c4017b9c937865f74f8f
vansh99-code <bansalvansh253@gmail.com>
2025-02-04 17:24:56 +0530

    context api project

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import UserContextProvider from './Context/UserContextProvider'
+import Login from './Components/Login'
+import Profile from './Components/Profile'
+
+function App() {
+ 
+  return (
+    <UserContextProvider>
+      <h1>hello, how are you ?</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
